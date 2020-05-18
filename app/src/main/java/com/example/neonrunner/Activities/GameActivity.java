3e05256bf40659f68a250a8c8f9c26b42d7fe688
ftelnov <fntelnovfn@gmail.com>
2020-05-18 13:52:15 +0300

    Fixed game page's orientation, added progress bars for level loading

--- app/src/main/java/com/example/neonrunner/Activities/GameActivity.java
+++ app/src/main/java/com/example/neonrunner/Activities/GameActivity.java
@@ -3,14 +3,23 @@ package com.example.neonrunner.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.neonrunner.R;
 
 public class GameActivity extends AppCompatActivity {
+    String level_raw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
+        Bundle bundle = getIntent().getExtras();
+        assert bundle != null;
+        level_raw = bundle.getString("level");
     }
 }
--- app/src/main/java/com/example/neonrunner/Fragments/MainFragment.java
+++ app/src/main/java/com/example/neonrunner/Fragments/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.neonrunner.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
@@ -7,12 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.neonrunner.Activities.GameActivity;
 import com.example.neonrunner.Activities.MainActivity;
 import com.example.neonrunner.R;
 import com.example.neonrunner.RetroShit.DataService;
@@ -49,16 +52,21 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button startButton = view.findViewById(R.id.startLevelButton);
+        final ProgressBar loadingbar = view.findViewById(R.id.loadingBar);
         final EditText inputField = view.findViewById(R.id.lvlUrl);
         final MainActivity activity = (MainActivity) getActivity();
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                loadingbar.setVisibility(ProgressBar.VISIBLE);
                 RetroInstance.getRetrofitInstance().create(DataService.class).getRawPaste(inputField.getText().toString()).enqueue(new Callback<ResponseBody>() {
                     @Override
                     public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        loadingbar.setVisibility(ProgressBar.GONE);
                         try {
-                            activity.showPrimaryToast(response.body().string());
+                            Intent intent = new Intent(activity, GameActivity.class);
+                            intent.putExtra("level", response.body().string());
+                            startActivity(intent);
                         } catch (IOException e) {
                             activity.showPrimaryToast(getString(R.string.connection_unstable));
                         }
@@ -66,6 +74,7 @@ public class MainFragment extends Fragment {
 
                     @Override
                     public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        loadingbar.setVisibility(ProgressBar.GONE);
                         activity.showPrimaryToast(getString(R.string.connection_unstable));
                     }
                 });
